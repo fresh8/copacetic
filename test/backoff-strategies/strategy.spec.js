2b627b81d391333730f0d73f203e3c9d66a8fc39
Toby Coleman <tobych82@gmail.com>
2017-07-07 11:45:21 +0100

    added max delay to backoff strategies

--- test/backoff-strategies/strategy.spec.js
+++ test/backoff-strategies/strategy.spec.js
@@ -6,7 +6,7 @@ describe('strategy', () => {
   const strategy = new Strategy()
 
   it('should fail after 0.005 seconds by default', () => {
-    expect(strategy.failAfter).to.equal(5)
+    expect(strategy.failAfter).to.equal(3)
   })
 
   it('should have an execute function', () => {
